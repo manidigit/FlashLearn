964a9351fbd6bb03a2c5f0404e0ed84a129cf6e8
manidigit <manidigit@gmail.com>
2026-09-13 09:36:41 +0330

    chore(v5.01): bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "5.00"
+        versionName = "5.01"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures { compose = true }
