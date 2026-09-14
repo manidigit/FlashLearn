0c86259f39c64a58739406397098b5a80ccc5d12
manidigit <manidigit@gmail.com>
2026-09-14 20:28:44 +0330

    chore: bump app to v5.68

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.flashlearn.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 67
-        versionName = "5.67"
+        versionCode = 68
+        versionName = "5.68"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
