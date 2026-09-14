4b8d37fd6a9ebbd1dabc653a2d7f055510f95dc2
manidigit <manidigit@gmail.com>
2026-09-14 10:06:12 +0330

    fix: remove unsupported BackHandler from MainActivity

--- app/src/main/java/com/flashlearn/app/MainActivity.kt
+++ app/src/main/java/com/flashlearn/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.flashlearn.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.compose.BackHandler
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
@@ -63,7 +62,6 @@ class MainActivity : ComponentActivity() {
     @Composable
     private fun AppRootScreen() {
         val uiState by appViewModel.state
-        BackHandler(enabled = uiState.selectedRoute != AppRoutes.HOME) { appViewModel.goBack() }
         val topLevel = uiState.selectedRoute in setOf(AppRoutes.HOME, AppRoutes.REVIEW, AppRoutes.LIBRARY, AppRoutes.PROGRESS, AppRoutes.SETTINGS)
         if (topLevel) {
             FlashLearnShell(selectedRoute = uiState.selectedRoute, onNavigate = { route ->
