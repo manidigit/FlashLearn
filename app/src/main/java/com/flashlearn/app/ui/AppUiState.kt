21caa107775939fd24d97369a7a79d5b7b48e35d
manidigit <manidigit@gmail.com>
2026-09-14 10:59:25 +0330

    fix: add functional app layout direction setting

--- app/src/main/java/com/flashlearn/app/ui/AppUiState.kt
+++ app/src/main/java/com/flashlearn/app/ui/AppUiState.kt
@@ -3,11 +3,13 @@ package com.flashlearn.app.ui
 import com.flashlearn.app.navigation.AppRoutes
 
 enum class AppearanceMode { SYSTEM, LIGHT, DARK }
+enum class AppLayoutDirection { RTL, LTR }
 
 data class AppUiState(
     val selectedRoute: String = AppRoutes.HOME,
     val selectedConceptId: java.util.UUID? = null,
-    val appearance: AppearanceMode = AppearanceMode.SYSTEM
+    val appearance: AppearanceMode = AppearanceMode.SYSTEM,
+    val layoutDirection: AppLayoutDirection = AppLayoutDirection.RTL
 ) {
     init {
         require(selectedRoute in AppRoutes.all()) { "Unknown application route: $selectedRoute" }
