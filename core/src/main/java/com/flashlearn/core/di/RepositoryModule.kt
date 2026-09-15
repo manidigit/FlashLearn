684a24afc4fc7386128563692d1ba55ff0356d7e
manidigit <manidigit@gmail.com>
2026-09-15 08:55:39 +0330

    v5.74: bind multi-format export repository

--- core/src/main/java/com/flashlearn/core/di/RepositoryModule.kt
+++ core/src/main/java/com/flashlearn/core/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.flashlearn.core.di
 
 import com.flashlearn.data.repository.*
+import com.flashlearn.data.backup.DataExportRepositoryImpl
 import com.flashlearn.domain.repository.*
 import dagger.Binds
 import dagger.Module
@@ -11,23 +12,24 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    @Binds @Singleton abstract fun bindConceptRepository(impl: RoomConceptRepository): ConceptRepository
-    @Binds @Singleton abstract fun bindContentRepository(impl: RoomContentRepository): ContentRepository
-    @Binds @Singleton abstract fun bindLearningStateRepository(impl: RoomLearningStateRepository): LearningStateRepository
-    @Binds @Singleton abstract fun bindDifficultyStateRepository(impl: RoomDifficultyStateRepository): DifficultyStateRepository
-    @Binds @Singleton abstract fun bindConceptTagRepository(impl: RoomConceptTagRepository): ConceptTagRepository
-    @Binds @Singleton abstract fun bindTagRepository(impl: RoomTagRepository): TagRepository
-    @Binds @Singleton abstract fun bindReviewHistoryRepository(impl: RoomReviewHistoryRepository): ReviewHistoryRepository
-    @Binds @Singleton abstract fun bindReviewSessionRepository(impl: RoomReviewSessionRepository): ReviewSessionRepository
-    @Binds @Singleton abstract fun bindSettingsRepository(impl: RoomSettingsRepository): SettingsRepository
-    @Binds @Singleton abstract fun bindCategoryRepository(impl: RoomCategoryRepository): CategoryRepository
-    @Binds @Singleton abstract fun bindParserMetadataRepository(impl: RoomParserMetadataRepository): ParserMetadataRepository
-    @Binds @Singleton abstract fun bindAchievementRepository(impl: RoomAchievementRepository): AchievementRepository
-    @Binds @Singleton abstract fun bindBackupRepository(impl: com.flashlearn.data.backup.RoomBackupRepository): BackupRepository
-    @Binds @Singleton abstract fun bindFlashLearnDatabase(impl: FlashLearnDatabaseImpl): FlashLearnDatabase
-    @Binds @Singleton abstract fun bindDataVersionRepository(impl: RoomDataVersionRepository): DataVersionRepository
-    @Binds @Singleton abstract fun bindVocabularyRelationRepository(impl: RoomVocabularyRelationRepository): VocabularyRelationRepository
-    @Binds @Singleton abstract fun bindVocabularyVariantRepository(impl: RoomVocabularyVariantRepository): VocabularyVariantRepository
-    @Binds @Singleton abstract fun bindReviewQueueRepository(impl: RoomReviewQueueRepository): ReviewQueueRepository
-    @Binds @Singleton abstract fun bindLanguageRepository(impl: RoomLanguageRepository): LanguageRepository
+ @Binds @Singleton abstract fun bindConceptRepository(i:RoomConceptRepository):ConceptRepository
+ @Binds @Singleton abstract fun bindContentRepository(i:RoomContentRepository):ContentRepository
+ @Binds @Singleton abstract fun bindLearningStateRepository(i:RoomLearningStateRepository):LearningStateRepository
+ @Binds @Singleton abstract fun bindDifficultyStateRepository(i:RoomDifficultyStateRepository):DifficultyStateRepository
+ @Binds @Singleton abstract fun bindConceptTagRepository(i:RoomConceptTagRepository):ConceptTagRepository
+ @Binds @Singleton abstract fun bindTagRepository(i:RoomTagRepository):TagRepository
+ @Binds @Singleton abstract fun bindReviewHistoryRepository(i:RoomReviewHistoryRepository):ReviewHistoryRepository
+ @Binds @Singleton abstract fun bindReviewSessionRepository(i:RoomReviewSessionRepository):ReviewSessionRepository
+ @Binds @Singleton abstract fun bindSettingsRepository(i:RoomSettingsRepository):SettingsRepository
+ @Binds @Singleton abstract fun bindCategoryRepository(i:RoomCategoryRepository):CategoryRepository
+ @Binds @Singleton abstract fun bindParserMetadataRepository(i:RoomParserMetadataRepository):ParserMetadataRepository
+ @Binds @Singleton abstract fun bindAchievementRepository(i:RoomAchievementRepository):AchievementRepository
+ @Binds @Singleton abstract fun bindBackupRepository(i:com.flashlearn.data.backup.RoomBackupRepository):BackupRepository
+ @Binds @Singleton abstract fun bindFlashLearnDatabase(i:FlashLearnDatabaseImpl):FlashLearnDatabase
+ @Binds @Singleton abstract fun bindDataVersionRepository(i:RoomDataVersionRepository):DataVersionRepository
+ @Binds @Singleton abstract fun bindVocabularyRelationRepository(i:RoomVocabularyRelationRepository):VocabularyRelationRepository
+ @Binds @Singleton abstract fun bindVocabularyVariantRepository(i:RoomVocabularyVariantRepository):VocabularyVariantRepository
+ @Binds @Singleton abstract fun bindReviewQueueRepository(i:RoomReviewQueueRepository):ReviewQueueRepository
+ @Binds @Singleton abstract fun bindLanguageRepository(i:RoomLanguageRepository):LanguageRepository
+ @Binds @Singleton abstract fun bindDataExportRepository(i:DataExportRepositoryImpl):DataExportRepository
 }
