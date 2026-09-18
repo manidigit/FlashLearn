d740cd7570f247a1330d24218f9f65ae621f2412
manidigit <manidigit@gmail.com>
2026-09-18 22:37:19 +0330

    v5.98 fix domain Gradle Kotlin DSL

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,6 +2,5 @@ plugins { id("com.android.library");  }
 android {
     namespace = "com.flashlearn.domain"; compileSdk = 34; defaultConfig { minSdk = 26 }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-    kotlinOptions { jvmTarget = "17" }
 }
 dependencies { implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3"); implementation("javax.inject:javax.inject:1"); testImplementation("junit:junit:4.13.2") }
