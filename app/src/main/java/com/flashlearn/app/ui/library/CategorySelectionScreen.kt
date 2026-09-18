121208eca7f035f74c229f37af19dc68e1fbd2b6
manidigit <manidigit@gmail.com>
2026-09-18 22:35:08 +0330

    v5.98 migrate UI icon parameters to MDI IIcon

--- app/src/main/java/com/flashlearn/app/ui/library/CategorySelectionScreen.kt
+++ app/src/main/java/com/flashlearn/app/ui/library/CategorySelectionScreen.kt
@@ -1,5 +1,7 @@
 package com.flashlearn.app.ui.library
 
+import com.flashlearn.app.ui.icons.Icon
+import com.mikepenz.iconics.typeface.IIcon
 import com.flashlearn.app.ui.icons.mdiIcon
 
 import androidx.compose.foundation.layout.*
