dbbf308b79a658131d10d24cb92a3f040d5bde8c
manidigit <manidigit@gmail.com>
2026-09-22 17:41:38 +0330

    chore: align app/src/androidTest/java/com/flashlearn/app/RuntimeGatePreflightTest.kt with v6.04

--- app/src/androidTest/java/com/flashlearn/app/RuntimeGatePreflightTest.kt
+++ app/src/androidTest/java/com/flashlearn/app/RuntimeGatePreflightTest.kt
@@ -18,7 +18,7 @@ class RuntimeGatePreflightTest {
     @Test fun releaseVersionMatchesCurrentBuild() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-        assertEquals("6.03", packageInfo.versionName)
-        assertEquals(103, packageInfo.longVersionCode.toInt())
+        assertEquals("6.04", packageInfo.versionName)
+        assertEquals(104, packageInfo.longVersionCode.toInt())
     }
 }
