424694a9cdcc251c2d750e143f6b9bc23bad143f
manidigit <manidigit@gmail.com>
2026-09-22 17:00:31 +0330

    chore: advance app release identity to 6.03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flashlearn.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 101
-        versionName = "6.01"
+        versionCode = 103
+        versionName = "6.03"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "APP_AUTHOR", "\"FlashLearn\"")
