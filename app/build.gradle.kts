5c0aba26086b8faff7961359681a9ea81be5a090
manidigit <manidigit@gmail.com>
2026-09-22 19:43:45 +0330

    chore: advance app version for Review RTL correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flashlearn.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 106
-        versionName = "6.06"
+        versionCode = 107
+        versionName = "6.07"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "APP_AUTHOR", "\"FlashLearn\"")
