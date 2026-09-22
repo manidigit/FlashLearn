66efafa8dc32607e1acb690c34350bf2d04b7842
manidigit <manidigit@gmail.com>
2026-09-22 10:00:47 +0330

    ci: add Google-hosted Maven Central mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,18 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        google()
+        maven { url = uri("https://maven-central.storage-download.googleapis.com/maven2/") }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        maven { url = uri("https://maven-central.storage-download.googleapis.com/maven2/") }
+        mavenCentral()
+    }
 }
 rootProject.name = "FlashLearn"
 include(":app", ":core", ":domain", ":database", ":data")
