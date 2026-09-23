3a62e14090f9b578a991e4bfce6bfe973a9cb615
manidigit <manidigit@gmail.com>
2026-09-23 19:09:58 +0330

    chore: reconcile v6.15 release and process records

--- app/src/androidTest/java/com/flashlearn/app/RuntimeGatePreflightTest.kt
+++ app/src/androidTest/java/com/flashlearn/app/RuntimeGatePreflightTest.kt
@@ -18,7 +18,7 @@ class RuntimeGatePreflightTest {
     @Test fun releaseVersionMatchesCurrentBuild() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-        assertEquals("6.14", packageInfo.versionName)
-        assertEquals(114, packageInfo.longVersionCode.toInt())
+        assertEquals("6.15", packageInfo.versionName)
+        assertEquals(115, packageInfo.longVersionCode.toInt())
     }
 }
