eeba48fd3f6009047719e76d9d19bdd33f78c94f
manidigit <manidigit@gmail.com>
2026-09-23 11:28:37 +0330

    chore: bump app to v6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flashlearn.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 109
-        versionName = "6.09"
+        versionCode = 110
+        versionName = "6.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "APP_AUTHOR", "\"FlashLearn\"")
