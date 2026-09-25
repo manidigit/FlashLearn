1b868438ed1ad61a5eb2d351d00e6be9584dbd1b
manidigit <manidigit@gmail.com>
2026-09-25 14:50:18 +0330

    test: use isolated activity for direction matrix

--- app/src/androidTest/java/com/flashlearn/app/DirectionThemeMatrixTest.kt
+++ app/src/androidTest/java/com/flashlearn/app/DirectionThemeMatrixTest.kt
@@ -21,7 +21,7 @@ import org.junit.Test
 
 class DirectionThemeMatrixTest {
     @get:Rule
-    val composeRule = createAndroidComposeRule<MainActivity>()
+    val composeRule = createAndroidComposeRule<ComposeTestActivity>()
 
     @Test
     fun rtlLtrAndLightDarkMatrixPreservesRootDirectionAndTheme() {
