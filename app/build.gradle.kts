2a09c6cc4a314740cc07cd778717a6a888906e68
manidigit <manidigit@gmail.com>
2026-09-25 12:18:48 +0330

    fix: restore Compose and Android test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,10 @@ dependencies {
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
